7e204f1061d63a599354262dd7d8beb7bc45b9c9
EricTalv <eric.talviste@tptlive.ee>
2021-04-17 01:52:18 +0300

    added smooth scroll

--- pages/index.vue
+++ pages/index.vue
@@ -19,17 +19,17 @@
           </p>
           <hr class="my-3">
           <div class="intro-nav flex justify-between">
-            <a href="">
-              <p class="intro-nav-item text-gray-800">
-                portfolio
-              </p>
-            </a>
-            <a href="">
+            <a  href="#about">
               <p class="intro-nav-item text-gray-800">
                 about
               </p>
             </a>
-            <a href="">
+            <a href="#portfolio">
+              <p class="intro-nav-item text-gray-800">
+                portfolio
+              </p>
+            </a>
+            <a href="#contact">
               <p class="intro-nav-item text-gray-800">
                 contact
               </p>
@@ -42,11 +42,11 @@
 
 
     <!--    About Section Start -->
-    <div class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+    <div id="about" class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
       <div class="about-section-wrapper h-full flex flex-wrap content-center">
         <div class="about-nav w-full flex">
-          <a class="text-gray-800" href="">portfolio</a>
-          <a class="text-gray-800 mx-5" href="">contact</a>
+          <a class="text-gray-800" href="#portfolio">portfolio</a>
+          <a class="text-gray-800 mx-5" href="#contact">contact</a>
           <p class="text-right w-full">About</p>
         </div>
         <hr class="my-2 w-full">
@@ -73,11 +73,11 @@
     <!--    About Section End -->
 
     <!--    Portfolio Section Start -->
-    <div class="portfolio-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+    <div id="portfolio" class="portfolio-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
       <div class="portfolio-section-wrapper h-full flex flex-wrap content-center">
         <div class="portfolio-nav w-full flex">
-          <a class="text-gray-800" href="">about</a>
-          <a class="text-gray-800 mx-5" href="">contact</a>
+          <a class="text-gray-800" href="#about">about</a>
+          <a class="text-gray-800 mx-5" href="#contact">contact</a>
           <p class="text-right w-full">Portfolio</p>
         </div>
         <hr class="my-2 w-full">
@@ -85,6 +85,7 @@
           <div class="portfolio-paragraph-section my-4">
             <p class="text-lg">This is the portfolio section</p>
             <p>hello.</p>
+
           </div>
 
         </div>
@@ -94,11 +95,11 @@
     <!--    Portfolio Section End -->
 
     <!--    Contact Section Start -->
-    <div class="contact-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+    <div id="contact" class="contact-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
       <div class="contact-section-wrapper h-full flex flex-wrap content-center">
         <div class="contact-nav w-full flex">
-          <a class="text-gray-800" href="">about</a>
-          <a class="text-gray-800 mx-5" href="">portfolio</a>
+          <a class="text-gray-800" href="#about">about</a>
+          <a class="text-gray-800 mx-5" href="#portfolio">portfolio</a>
           <p class="text-right w-full">Contact</p>
         </div>
         <hr class="my-2 w-full">
@@ -132,7 +133,7 @@
       let navItem = baffle('.intro-nav-item');
       navItem.reveal(3500);
 
-    }
+    },
 
   }
 </script>
@@ -144,6 +145,9 @@
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
   }
   */
+
+  .content-container {
+  }
 
   .about-paragraph-section {
     p {
