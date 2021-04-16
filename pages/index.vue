d35d514c35ad16a9de0aa7b385240cb192946507
EricTalv <eric.talviste@tptlive.ee>
2021-04-16 22:22:51 +0300

    setup webpage flow, fixed intro section

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content-container container mx-auto h-screen">
     <!--    Welcome Section start -->
-    <div class="intro-container">
+    <div class="intro-container grid grid-cols-1 h-full place-content-center ">
 
       <sphere></sphere>
       <div class="flex flex-col items-center">
@@ -15,7 +15,7 @@
             I am Eric Talviste.
           </p>
           <p class="intro-text">
-            I develop websites, music and create digital art.
+            I develop websites, music and create digital art. 
           </p>
           <hr class="my-3">
           <div class="intro-nav flex justify-between">
@@ -41,29 +41,100 @@
     <!--    Welcome Section End -->
 
     <!--    About Section Start -->
-    <div class="about-section h-screen my-auto mx-auto w-3/5 text-xs sm:text-sm md:text-base">
-      <div class="about-nav flex">
-        <a class="text-gray-800" href="">portfolio</a>
-        <a class="text-gray-800 mx-5" href="">contact</a>
-        <p class="text-right w-full">About</p>
-      </div>
-      <hr class="my-2">
-      <div class="about-text">
-        <div class="about-paragraph-section my-4">
-          <p class="text-lg">Who am I exactly?</p>
-          <p>That's a good question. I don't know.</p>
+<!--    <div class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">-->
+<!--      <div class="about-section-wrapper h-full flex flex-wrap content-center">-->
+
+<!--          <sphere style="width: 130%"></sphere>-->
+
+<!--        <div class="about-text">-->
+<!--          <div class="">-->
+<!--            <p>I am Eric Talviste.</p>-->
+<!--            <p>I develop websites, music and create digital art.</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--        <hr class="my-2 w-full">-->
+<!--        <div class="about-nav w-full flex justify-between">-->
+<!--          <a class="text-gray-800" href="">portfolio</a>-->
+<!--          <a class="text-gray-800 mx-5" href="">about</a>-->
+<!--          <a class="text-gray-800">contact</a>-->
+<!--        </div>-->
+
+<!--      </div>-->
+<!--    </div>-->
+    <!--    About Section End -->
+
+    <!--    About Section Start -->
+    <div class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+      <div class="about-section-wrapper h-full flex flex-wrap content-center">
+        <div class="about-nav w-full flex">
+          <a class="text-gray-800" href="">portfolio</a>
+          <a class="text-gray-800 mx-5" href="">contact</a>
+          <p class="text-right w-full">About</p>
         </div>
-        <div class="about-paragraph-section">
-          <p class="text-lg">What can I do?</p>
-          <p>I work with all things related to websites and web development. To me, websites aren’t just a nice interface for information,  a website can be so much more.</p>
-          <p>It is a piece of interactive art, you can make it bend and twirl as you want and it all comes together as something useful. </p>
-          <p>I create digital art, I very much enjoy the beauty in chaos, dystpoianism, brutalism, all things grunge and out of line. </p>
-          <p>I also play the guitar and write music.</p>
+        <hr class="my-2 w-full">
+        <div class="about-text">
+          <div class="about-paragraph-section my-4">
+            <p class="text-lg">Who am I exactly?</p>
+            <p>That's a good question. I don't know.</p>
+          </div>
+          <div class="about-paragraph-section">
+            <p class="text-lg">What can I do?</p>
+            <p>I work with all things related to websites and web development. To me, websites aren’t just a nice
+              interface for information, a website can be so much more.</p>
+            <p>It is a piece of interactive art, you can make it bend and twirl as you want and it all comes together as
+              something useful. </p>
+            <p>I create digital art, I very much enjoy the beauty in chaos, dystpoianism, brutalism, all things grunge
+              and out of line. </p>
+            <p>I also play the guitar and write music.</p>
+          </div>
+
         </div>
 
       </div>
     </div>
     <!--    About Section End -->
+
+  <!--    Portfolio Section Start -->
+    <div class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+      <div class="about-section-wrapper h-full flex flex-wrap content-center">
+        <div class="about-nav w-full flex">
+          <a class="text-gray-800" href="">about</a>
+          <a class="text-gray-800 mx-5" href="">contact</a>
+          <p class="text-right w-full">Portfolio</p>
+        </div>
+        <hr class="my-2 w-full">
+        <div class="about-text">
+          <div class="about-paragraph-section my-4">
+            <p class="text-lg">This is the portfolio section</p>
+            <p>hello.</p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <!--    Portfolio Section End -->
+
+  <!--    Contact Section Start -->
+    <div class="about-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
+      <div class="about-section-wrapper h-full flex flex-wrap content-center">
+        <div class="about-nav w-full flex">
+          <a class="text-gray-800" href="">about</a>
+          <a class="text-gray-800 mx-5" href="">portfolio</a>
+          <p class="text-right w-full">Contact</p>
+        </div>
+        <hr class="my-2 w-full">
+        <div class="about-text">
+          <div class="about-paragraph-section my-4">
+            <p class="text-lg">This is the Contact section</p>
+            <p>hello.</p>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+    <!--    Contact Section End -->
 
   </div>
 </template>
