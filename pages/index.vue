7244910e178b27fd8ff53d82f016208dd2e04219
erictalv <eric.talviste@tptlive.ee>
2021-04-24 13:52:22 +0300

    portfolio flow is working

--- pages/index.vue
+++ pages/index.vue
@@ -72,8 +72,11 @@
 
     <!--    Portfolio Section Start -->
     <div id="portfolio" class="portfolio-section my-24 h-screen mx-auto w-3/5 text-xs sm:text-sm md:text-base">
-      <div class="portfolio-section-wrapper h-full w-full flex flex-row flex-nowrap overflow-x-auto" >
+      <div class="portfolio-section-wrapper h-full w-full flex flex-row flex-nowrap overflow-x-hidden" >
         <div id="portfolioList" class="portfolio-sec-wrapper min-w-full flex flex-wrap content-center">
+
+          <nuxt-content :document="portfolio"></nuxt-content>
+
 
           <div class="portfolio-nav w-full flex">
             <a class="text-gray-800" href="#about">about</a>
@@ -91,13 +94,15 @@
               </li>
 
               <li v-for="portfolioItem of portfolio" :key="portfolioItem.slug">
-                <nuxt-link @click="setCurrentPortfolioItem(portfolioItem)" :to="`${portfolioItem.slug}`">
+                <a @click="setCurrentPortfolioItem(portfolioItem)" :href="`#${portfolioItem.slug}`">
 
-                  <p class="title">{{ portfolioItem.slug }}</p>
+                  <p class="title">{{ portfolioItem.title }}</p>
                   <p class="portfolio-item-description text-xs">{{ portfolioItem.description }}</p>
 
-                </nuxt-link>
+                </a>
               </li>
+
+
             </ul>
           </div>
         </div>
@@ -107,11 +112,11 @@
             <a class="text-gray-800" href="#portfolioList"><-</a>
             <a class="text-gray-800 mx-5" href="#about">about</a>
             <a class="text-gray-800 " href="#contact">contact</a>
-            <p class="text-right w-full">Project Item</p>
+            <p class="text-right w-full">{{ currentPortfolioItem.title }}</p>
           </div>
           <hr class="my-2 w-full">
           <div class="portfolio-content py-2 px-10">
-            content
+            {{ currentPortfolioItem.body }}
           </div>
         </div>
       </div>
@@ -150,18 +155,22 @@ export default {
 
   data() {
     return {
-      currentPortfolioItem: []
-    }
-  },
+      currentPortfolioItem: {},
 
-  // Fetch all data from Portfolio
-  async asyncData({$content, params}) {
-    const portfolio = await $content('portfolio')
-      //.only(['title', 'description', 'slug'])
-      .fetch()
-
-    return {
-      portfolio
+      portfolio: [
+        {
+          slug: "my-project",
+          title: "My Project",
+          description: "This is my project small description",
+          body: "This is the body of my project"
+        },
+        {
+          slug: "second-project",
+          title: "Second Project",
+          description: "Second projects dsecription is slightly longer than usual",
+          body: "Body should contain more variety of text so I am not sure what to do here"
+        },
+      ]
     }
   },
 
@@ -170,7 +179,7 @@ export default {
     setCurrentPortfolioItem(portfolioItem){
 
         this.currentPortfolioItem = portfolioItem;
-
+        console.log(this.currentPortfolioItem);
     }
 
   },
