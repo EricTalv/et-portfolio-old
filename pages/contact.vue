83c3cfffa5e4a04d93c3c94283ca5fa82bc38f16
erictalv <eric.talviste@tptlive.ee>
2021-11-16 13:49:22 +0200

    rollback to contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,9 +1,10 @@
 <template>
   <!--    Contact Section Start -->
 
-  <div class="about-section flex flex-wrap  justify-center">
-    <div class="about-wrapper mt-44 w-3/5 c-min-width-about">
-      <div class="about-nav w-full flex">
+
+  <div class="contact-section flex flex-wrap content-center justify-center">
+    <div class="contact-wrapper w-3/5 c-min-width-contact">
+      <div class="contact-nav w-full flex">
         <a href="#">portfolio</a>
         <a class="mx-3" href="#">about</a>
         <p class="text-right w-full">Contact</p>
@@ -11,7 +12,7 @@
 
       <hr class="my-2 mb-16">
 
-      <div class="about-body flex  mx-auto justify-center flex-col sm:flex-row">
+      <div class="contact-body flex  mx-auto justify-center flex-col sm:flex-row">
 
         <div class="contact-info">
           <div class="contact-info-text">
@@ -28,7 +29,20 @@
           </div>
         </div>
 
+        <div class="contact-spacer w-16 hidden sm:flex"></div>
 
+        <div class="contact-links border border-white w-52 hidden sm:flex flex-wrap flex-col content-center justify-center">
+          <a href="https://github.com/EricTalv" target="_blank">GitHub</a>
+          <a class="my-3" href="https://www.linkedin.com/feed/" target="_blank">LinkedIn</a>
+          <a href="https://www.instagram.com/et_experience/" target="_blank">Design</a>
+        </div>
+
+
+        <div class="contact-links-mobile border-t mt-16 flex justify-around sm:hidden">
+          <a href="https://github.com/EricTalv" target="_blank">GitHub</a>
+          <a class="mx-3" href="https://www.linkedin.com/feed/" target="_blank">LinkedIn</a>
+          <a href="https://www.instagram.com/et_experience/" target="_blank">Design</a>
+        </div>
 
       </div>
     </div>
@@ -40,3 +54,11 @@
 
 </template>
 
+
+<style>
+
+.c-min-width-contact {
+  min-width: 366px;
+}
+
+</style>
