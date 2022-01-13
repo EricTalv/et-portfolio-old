654f40ad3ff238e5d3b6067c631c0c491425b483
erictalv <eric.talviste@tptlive.ee>
2022-01-13 16:01:11 +0200

    maintenence wall

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Hello.</h1>
+    <h2>This site is work in progress.</h2>
+    <h3>Contact me -> eric . talviste [at] gmail . com</h3>
+  </main>
+</template>
+
+<script>
+export default {
+name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
