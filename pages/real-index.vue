72ab272c972e74960b6d720ef540408595bbf3df
erictalv <eric.talviste@tptlive.ee>
2022-01-13 17:04:17 +0200

    index bg

---
+++ pages/real-index.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="index">
+    <div class="index__left-panel"></div>
+    <div class="index__right-panel"></div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "real-index"
+}
+</script>
+
+<style lang="scss" scoped>
+
+  //  Index background
+  .index {
+
+    width: 100%;
+
+    background-color: black;
+    background-image: url("./assets/images/index-mobile-bg.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+
+  }
+
+  /* BreakPoints */
+  $breakpoint-tablet: 768px;
+  $breakpoint-mobile: 501px;
+
+  // Index Background media queries
+  @media  (min-width: $breakpoint-mobile) {
+    .index {
+      background-image: url("./assets/images/index-bg.png");
+
+    }
+  }
+
+
+
+
+</style>
